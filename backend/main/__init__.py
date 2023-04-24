9ea3925159c24d25a6506c193dd7a3b24aa387ba
perezmatias <maag.perez@alumno.um.edu.ar>
2023-04-24 08:50:04 -0300

    cuarto commit

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -2,14 +2,25 @@ import os
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 import main.resources as resources
 api = Api()
+db = SQLAlchemy()
 #vamos a crear un metodo que inicializara la app y todos los modulos
 def create_app():
     #inicio flask
     app = Flask(__name__)
     #variables de entorno
     load_dotenv()
+
+    if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+        db.init_app(app)
+        
+        import main.resources as resources
+
     api.add_resource(resources.UsuariosResource,"/usuarios")
 
     api.add_resource(resources.UsuarioResource, "/usuario/<id>")
