6f8c2e3bcb55c8984cd39aed8dfbe70731f1c730
perezmatias <maag.perez@alumno.um.edu.ar>
2023-05-30 23:50:55 -0300

    tp7

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
 api = Api()
 db=SQLAlchemy()
@@ -61,5 +62,15 @@ def create_app():
 
     from main.auth import routes
     app.register_blueprint(routes.auth)
+
+    app.config['MAIL_HOSTNAME'] = os.getenv('MAIL_HOSTNAME')
+    app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
+    app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
+    app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
+    app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['FLASKY_MAIL_SENDER'] = os.getenv('FLASKY_MAIL_SENDER')
+    
+    mailsender.init_app(app)
 
     return app
